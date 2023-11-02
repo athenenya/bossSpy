137b290c675aaab0adb2c5631906b779c87ae75f
Athene <athenenyarai@gmail.com>
2023-11-02 06:37:43 +0200

    add tests

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -1,7 +1,11 @@
 import unittest
 from src import connections
 
-class testBossSpy(unittest):
+class testBossSpy(unittest.TestCase):
     def test_getEmails(self):
-        self.assertEqual(connections.get_unread_count(), 5, "Should be 5")
+        self.assertEqual(connections.get_unread_count(), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
